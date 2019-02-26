cfef0df2de80c2e3c81b78216feba5c89d3e9cb9
zhou <3192713828@qq.com>
2019-02-26 21:41:50 +0800

    '查询分类的接口'

--- routes/billApi/index.js
+++ routes/billApi/index.js
@@ -41,6 +41,7 @@ var addBill = function(req, res, next) {
         }
 
         function addbill() {
+            params.time = new Date(params.time);
             mongodb.insert(dbBase, dbbillColl, params, function(result) {
                 if (result) {
                     res.send({ code: 0, message: "插入成功" });
@@ -53,9 +54,13 @@ var addBill = function(req, res, next) {
 
     }
     //查询账单
+
+// 年 + 分类
+// 月 + 分类
 var getBill = function(req, res, next) {
     var params = req.query,
         uid = params.uid,
+
         time = params.time; //2019 2019-02
     if (time.indexOf('-') != -1) { //月  2019-12   2020-01
         var timeArr = time.split('-'); // [2019,02]
@@ -69,16 +74,12 @@ var getBill = function(req, res, next) {
     }
     console.log(maxTime);
 
-    mongodb.find(dbBase, dbbillColl, { uid: uid, time: { $lt: maxTime, $gte: time } }, function(result) {
+    mongodb.find(dbBase, dbbillColl, { uid: uid, time: { $lt: new Date(maxTime), $gte: new Date(time) } }, function(result) {
         console.log(result);
         if (result.length > 0) {
             res.send({ code: 0, data: result });
         } else {
             res.send({ code: 1, message: "没有查询到账单" });
-        }
-    }, {
-        sort: {
-            time: -1
         }
     });
 }
